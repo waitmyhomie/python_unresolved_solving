4a543905da6b5f13369552790975619f2f51cfb5
alonadanich <alonadanich@gmail.com>
2024-11-05 19:31:09 +0100

    logic for notes

---
+++ src/interfaces/Notes.py
@@ -0,0 +1,35 @@
+class Notes:
+    def __init__(self):
+        self.notes = {}  # A dictionary for saving notes.
+        self.next_id = 1  # A counter for generating unique IDs for notes
+
+    def add_note(self, content):
+        # Adds a new note with the specified content
+        note_id = self.next_id
+        self.notes[note_id] = content
+        self.next_id += 1
+        print(f"The note is saved with ID: {note_id}.")
+
+    def edit_note(self, note_id, new_content):
+        # Edits an existing note by ID
+        if note_id in self.notes:
+            self.notes[note_id] = new_content
+            print(f"The note with ID {note_id} is updated.")
+        else:
+            print("The note with this ID is not found.")
+
+    def delete_note(self, note_id):
+        # Deletes the note by ID
+        if note_id in self.notes:
+            del self.notes[note_id]
+            print(f"The note with ID {note_id} is deleted.")
+        else:
+            print("The note with this ID is not found.")
+
+    def show_notes(self):
+        # Shows all the notes
+        if not self.notes:
+            print("There are no notes.")
+        else:
+            for note_id, content in self.notes.items():
+                print(f"ID: {note_id} - {content}")
